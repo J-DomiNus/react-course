651b68eef59ab1891daa155a5aebe09dcb2f519f
Dominus <joeru.desu@gmail.com>
2020-06-04 12:30:56 -0300

    Props excercises finished

--- src/App.js
+++ src/App.js
@@ -10,16 +10,26 @@ import './App.css';
 const Hello = (props) => <h2>{props.title}</h2>
 class Text extends Component {
   render () {
-    const txtBool = this.props.boolean ? 'True':'False'
-    const mappedNumbers = this.props.arrayOfNumbers.map(n=>n*2)
+    const {
+      arrayOfNumbers,
+      boolean,
+      multiply,
+      number,
+      objectWithInfo,
+      text,
+      title
+    } = this.props
+    const txtBool = boolean ? 'True':'False'
+    const mappedNumbers = arrayOfNumbers.map(multiply)
   return (
     <div>
-      <p>{this.props.text}</p>
-      <p>{this.props.number}</p>
+      {title}
+      <p>{text}</p>
+      <p>{number}</p>
       <p>{txtBool}</p>
-      <p>{this.props.arrayOfNumbers.join('-')}</p>
+      <p>{arrayOfNumbers.join('-')}</p>
       <p>{mappedNumbers.join(',')}</p>
-      <p>{this.props.objectWithInfo.key1}</p>
+      <p>{objectWithInfo.key1}</p>
     </div>
   )
   }
@@ -34,9 +44,11 @@ function App() {
       <Text 
         arrayOfNumbers={[2,5,3,7,4,5]}
         boolean
+        multiply={(number) => number * 3}
         number={5} 
         objectWithInfo={{ key1: 'value1', key2: 'value2'}}
-        text='Puto el que lee' />
+        text='Puto el que lee!' />
+        title={<h1>I'm a Genious</h1>}
     </div>
   );
 }
