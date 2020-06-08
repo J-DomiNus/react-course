e003c1fd936e5292dc7f0e0acd3faa377279e431
Dominus <joeru.desu@gmail.com>
2020-06-08 18:17:21 -0300

    trabajando con arrays y listas

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React, { Component } from 'react';
-import ConditionalSection from './sections/conditional'
+
 class App extends Component {
   render () {
+    const numbers = [1,1,3,4,6]
+
     return (
-      <div className="App">
-        <ConditionalSection />
+      <div className='App'>
+        <h4>Working on lists</h4>
+        {numbers.map((x, i)=>{
+          return <p key={i}>i'm number {x}</p>
+        })}
       </div>
-    );
+    )
   }
 }
 
---
+++ src/sections/conditional.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react'
+
+class LoginButton extends Component {
+    render () {
+        return <button>Iniciar Sesión</button>
+    }
+}
+class LogoutButton extends Component {
+    render () {
+        return (
+            <div>
+                <p>Bienvenido usuario!</p>
+                <button>Cerrar Sesión</button>
+            </div>
+            )
+    }
+}
+
+
+export default class ConditionalSection extends Component {
+    constructor() {
+        super()
+        this.state = { logStatus: true}
+    }
+    render () {
+        
+        return (
+            <div>
+                <h4>Conditional Rendering</h4>
+                {this.state.logStatus
+                ? <LogoutButton />
+                : <LoginButton />
+                }
+            </div>
+        )
+    }
+}
+
